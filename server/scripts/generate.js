0d920c0e5421462129be66908bc98afbdb494d9b
bocarw121 <bocaralhassanwane@gmail.com>
2023-11-10 08:56:44 -0500

    Run prettier

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,5 +1,5 @@
-const secp = require("ethereum-cryptography/secp256k1");
-const {toHex} = require("ethereum-cryptography/utils");
+const secp = require('ethereum-cryptography/secp256k1');
+const { toHex } = require('ethereum-cryptography/utils');
 //import { secp256k1 } from "ethereum-cryptography/secp256k1";
 //import { toHex } from "ethereum-cryptography/utils";
 
@@ -8,5 +8,5 @@ const privateKey = secp.secp256k1.utils.randomPrivateKey();
 
 const publicKey = secp.secp256k1.getPublicKey(privateKey);
 
-console.log('private key',toHex(privateKey));
-console.log(`public key: ${toHex(publicKey)}`)
+console.log('private key', toHex(privateKey));
+console.log(`public key: ${toHex(publicKey)}`);
