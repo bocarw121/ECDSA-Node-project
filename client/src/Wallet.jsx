f6ab9d8f3b44b5bd3e7f97cb6cfb9fa0a041854b
bocarw121 <bocaralhassanwane@gmail.com>
2023-11-10 10:54:13 -0500

    Update Wallet

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,31 +1,11 @@
 import server from './server';
 
-function Wallet({ address, setAddress, balance, setBalance }) {
-  async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
-    if (address) {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
-    } else {
-      setBalance(0);
-    }
-  }
-
+function Wallet({ address, balance }) {
   return (
     <div className="container wallet">
       <h1>Your Wallet</h1>
 
-      <label>
-        Wallet Address
-        <input
-          placeholder="Type an address, for example: 0x1"
-          value={address}
-          onChange={onChange}
-        ></input>
-      </label>
+      <h4>Your wallet address is: {address}</h4>
 
       <div className="balance">Balance: {balance}</div>
     </div>
